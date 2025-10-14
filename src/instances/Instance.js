db1eecd9ba1b3e7e44cab3cb6788326657cb2d62
Nambi Nachiyar <nambi2731@gmail.com>
2025-10-14 11:58:37 +0530

    instance

--- src/instances/Instance.js
+++ src/instances/Instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_URL;
 const axiosInstance = axios.create({
-   baseURL,
+   baseURL:baseURL,
    headers:{
     'Content-Type':'application/json'
    }
